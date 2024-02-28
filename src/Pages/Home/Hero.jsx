2b6d88d679feaa27ad06aef55a5fecf18b1bf89a
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2024-02-28 11:33:12 +0000

    Working on Hero section.

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -1,5 +1,29 @@
 export default function Hero() {
     return (
-        <h1> Hero Section </h1>
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title">
+                        Hey, I'm Pete.
+                    </p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section--title--color">
+                            Full Stack
+                        </span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section--description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+
+                        <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium in quasi.
+                    </p>
+                </div>
+                <button className="btn btn-primary"> Download CV</button>
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/me.png" alt="me, the dev!"></img>
+            </div>
+        </section>
     )
 }
