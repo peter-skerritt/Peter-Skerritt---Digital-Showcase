cfc306cfa65996894e9f10aa4875bac08ccba097
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2024-04-26 10:50:15 +0100

    Added routes to App js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Pages/Home/Navbar';
 import Home from "./Pages/Homescreen";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}>
+
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
