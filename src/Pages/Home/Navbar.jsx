873133e232e3c3b9481d4777346a9f9bc2dd1bae
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2024-04-30 16:01:20 +0100

    Working on the nav content and setting up page links.

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -38,7 +38,59 @@ function Navbar() {
         <div>
             <img src="./img/logo.svg" alt="Logoipsum" />
         </div>
-        <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}></a>
+        <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}>
+            <span className="nav__hamburger__line"></span>
+            <span className="nav__hamburger__line"></span>
+            <span className="nav__hamburger__line"></span>
+        </a>
+        <div className={`navbar--items ${navActive ? "active" : ""}`}>
+            <ul>
+                <li>
+                    <Link onClick={closeMenu}
+                    activeClass="navBAr--active-content"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    to="heroSection"
+                    className="navbar--content"
+                    >Home</Link>
+                </li>
+                <li>
+                    <Link onClick={closeMenu}
+                    activeClass="navBAr--active-content"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    to="MyPortfolio"
+                    className="navbar--content"
+                    >Portfolio</Link>
+                </li>
+                <li>
+                    <Link onClick={closeMenu}
+                    activeClass="navBAr--active-content"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    to="AboutMe"
+                    className="navbar--content"
+                    >About Me</Link>
+                </li>
+                <li>
+                    <Link onClick={closeMenu}
+                    activeClass="navBAr--active-content"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    to="heroSection"
+                    className="navbar--content"
+                    >Contact</Link>
+                </li>
+            </ul>
+        </div>
        </nav>
     )
 }
