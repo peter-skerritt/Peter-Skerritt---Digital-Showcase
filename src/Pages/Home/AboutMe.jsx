7af30fcce6811ac469f07654d35c1dc2bbe52730
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2024-05-15 13:08:18 +0100

    Adding About Me Section.

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,9 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--sectio--image">
+                <img src="./img/about-me.png" alt="About Me"></img>
+            </div>
+        </section>
+    )
+} 
