4de9420b34083e56e0cbadf02948cc66505ab7e1
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2024-05-15 10:31:36 +0100

    Skills section being added, navbar complete.

---
+++ src/Pages/Home/Skills.jsx
@@ -0,0 +1,9 @@
+import data from "../../ data/index.json";
+
+export default function MySkills(){
+    return(
+        <section className="skills--section" id="mySkills">
+            <div></div>
+        </section>
+    )
+}
--- src/Pages/Homescreen/index.jsx
+++ src/Pages/Homescreen/index.jsx
@@ -1,9 +1,11 @@
 import Hero from "../Home/Hero";
+import MySkills from "../Home/Skills";
 
 export default function Home () {
     return (
         <>
-        <Hero/>
+        <Hero />
+        <MySkills />
         </>
     )
 }
